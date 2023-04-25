ef4a993b1cfddd6c6eca1fe362a6f5fecf15eb27
Nikith KB <90968115+nikithkb@users.noreply.github.com>
2023-04-25 11:35:13 +0530

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,6 +3,6 @@
 docker run -d -p 27017:27017 -v $(pwd)/data:/data/db --name mymongodb --network mongodb -e ALLOW_EMPTY_PASSWORD=yes bitnami/mongodb:latest
 
 
-docker run -d -p 5000:5000 --link mymongodb:mongo --network mongodb orders-docker 
-docker run -d -p 5050:5000 --link mymongodb:mongo --network mongodb products-docker 
-docker run -d -p 5100:5000 --link mymongodb:mongo --network mongodb users-docker 
+docker run -d -p 5000:5000 --link mymongodb:mongo --network mongodb order-docker 
+docker run -d -p 5050:5000 --link mymongodb:mongo --network mongodb product-docker 
+docker run -d -p 5100:5000 --link mymongodb:mongo --network mongodb user-docker 
